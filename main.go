6f5d1eb04bbbea5d8861425ce4ea9d30bde82d4b
mochi-yu <mochi.2225.it@gmail.com>
2023-09-05 01:39:29 +0900

    fix: 設定周りの不要な項目を削除

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/cocoide/commitify/cmd"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	viper.SetConfigType("yaml")
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
-		log.Println("An error occurred while reading the configuration file:", err)
-	}
-
 	cmd.Execute()
 }
