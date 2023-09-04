a62741bde935a82299c2905d4a3689c8deaea256
mochi-yu <mochi.2225.it@gmail.com>
2023-09-05 01:38:07 +0900

    feat: `config.yaml`ファイルが無ければ新規で作成する処理を追加

--- util/config.go
+++ util/config.go
@@ -3,6 +3,7 @@ package util
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/cocoide/commitify/internal/entity"
 	"github.com/spf13/viper"
@@ -10,36 +11,53 @@ import (
 
 func ReadConfig() (*entity.Config, error) {
 	var result entity.Config
+
+	// configファイルがあるかどうかを確認
+	_, err := os.Stat("config.yaml")
+	if os.IsNotExist(err) {
+		if _, err := os.Create("config.yaml"); err != nil {
+			return &result, fmt.Errorf("error creating config file, %s", err.Error())
+		}
+	}
+
 	viper.AddConfigPath(".")
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	if err := viper.ReadInConfig(); err != nil {
-		return &result, fmt.Errorf("Error reading config file, %s", err.Error())
+		return &result, fmt.Errorf("error reading config file, %s", err.Error())
 	}
 	if err := viper.Unmarshal(&result); err != nil {
-		return &result, fmt.Errorf("Unable to decode into struct, %v", err.Error())
+		return &result, fmt.Errorf("unable to decode into struct, %v", err.Error())
 	}
 	return &result, nil
 }
 
 func WriteConfig(config *entity.Config) error {
+	// configファイルがあるかどうかを確認
+	_, err := os.Stat("config.yaml")
+	if !os.IsNotExist(err) {
+		if _, err := os.Create("config.yaml"); err != nil {
+			return fmt.Errorf("error creating config file, %s", err.Error())
+		}
+	}
+
 	viper.AddConfigPath(".")
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	configMap := make(map[string]interface{})
 	configBytes, err := json.Marshal(config)
 	if err != nil {
-		return fmt.Errorf("Error marshalling config: %s", err.Error())
+		return fmt.Errorf("error marshalling config: %s", err.Error())
 	}
 	err = json.Unmarshal(configBytes, &configMap)
 	if err != nil {
-		return fmt.Errorf("Error unmarshalling config: %s", err.Error())
+		return fmt.Errorf("error unmarshalling config: %s", err.Error())
 	}
 	if err := viper.MergeConfigMap(configMap); err != nil {
 		return err
 	}
 	if err := viper.WriteConfig(); err != nil {
-		return fmt.Errorf("Error saving config file, %s", err.Error())
+		return fmt.Errorf("error saving config file, %s", err.Error())
 	}
 	return nil
 }
